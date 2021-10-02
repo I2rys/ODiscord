cee8c01b8c448b5494fcc471a4d64f5941c8d13c
I2rys <i2rys@protonmail.com>
2021-10-01 18:02:40 -0700

    Create master_card_cc.js

---
+++ ADS/plugins/master_card_cc.js
@@ -0,0 +1,13 @@
+//Main
+function self(message){
+    const is_mastercard_cc = message.content.match(/^5[1-5]\d{2}(| |-)(?:\d{4}\1){2}\d{4}$/gm)
+
+    if(is_discover_cc){
+        console.log(`Mastercard cc found: ${is_mastercard_cc.toString().replace(",", ", ")}`)
+    }
+}
+
+//Exporter
+module.exports = {
+    self: self
+}
